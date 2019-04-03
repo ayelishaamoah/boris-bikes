cce1221f7ccc5151ffa92a0f1a72086d9640b015
Student <student@Makerss-MacBook-Pro.local>
2019-04-03 15:44:14 +0100

    Added gets bike and bike working methods and tests

--- spec/boris_bikes_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'boris_bikes'
-
-describe DockingStation do
-  describe 'release_bike' do
-    it { is_expected.to respond_to(:release_bike) }
-  end
-end
---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,10 @@
+require 'docking_station'
+
+describe DockingStation do
+  it { is_expected.to respond_to(:release_bike) }
+
+  it 'releases a working bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+end
