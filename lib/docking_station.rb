25c73432f9494636da63a7f17909a88eb926cd50
Student <student@Makerss-MacBook-Pro.local>
2019-04-03 17:02:31 +0100

    Added dock method and rspec tests

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,14 @@
 require_relative 'bike'
 
 class DockingStation
+
   def release_bike
     Bike.new
   end
+
+  attr_reader :bike
+  def dock(bike)
+    @bike = bike   
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,4 +7,16 @@ describe DockingStation do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it { is_expected.to respond_to :dock }
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it { is_expected.to respond_to(:bike) }
+
+  it 'returns the docked bike' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
 end
